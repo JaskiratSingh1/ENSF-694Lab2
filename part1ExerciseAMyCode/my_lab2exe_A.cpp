691a6965dbf0f96f022aa0703e9a4c5c1b87fb2d
Jaskirat Singh <jsingh9@ualberta.ca>
2024-07-04 20:35:22 -0400

    my_strlen and my_strncat functions created and  implemented in the code.

--- part1ExerciseAMyCode/my_lab2exe_A.cpp
+++ part1ExerciseAMyCode/my_lab2exe_A.cpp
@@ -12,8 +12,16 @@ int my_strlen(const char *s);
  *     terminating null.
  */
 
-void my_strncat(char *dest, const char *source, int);
+void my_strncat(char *dest, const char *source, int numChars);
 /*  Duplicates strncat from <cstring>, except return type is void.
+ *  REQUIRES
+ *     dest points to the beginning of the new destination string.
+ *     source points to the beginning of the source string.
+ *     numChars specifies how many characters need to be concatinated
+ *  PROMISES
+ *     Copies numChar number of characters from dest to source string.
+ *     If the number of characters in source string are less than numChars,
+ *     then all the characters from source are copied to dest.
  */
 
 #include <iostream>
@@ -33,7 +41,7 @@ int main(void)
     int length;
     
     /* using strlen libarary function */
-    length = (int) strlen(my_string);
+    length = (int) my_strlen(my_string);
     cout << "\nLine 1: my_string length is " << length;
   
     /* using sizeof operator */
@@ -44,33 +52,33 @@ int main(void)
     strcpy(my_string, str1);
     cout << "\nLine 3: my_string contains: " << my_string;
    
-    length = (int) strlen(my_string);
+    length = (int) my_strlen(my_string);
     cout << "\nLine 4: my_string length is " << length << ".";
    
     my_string[0] = '\0';
     cout << "\nLine 5: my_string contains:\"" << my_string << "\"";
   
-    length = (int) strlen(my_string);
+    length = (int) my_strlen(my_string);
     cout << "\nLine 6: my_string length is " <<  length << ".";
    
     bytes = sizeof (my_string);
     cout << "\nLine 7: my_string size is still " << bytes << " bytes.";
  
     /* strncat append the first 3 characters of str5 to the end of my_string */
-    strncat(my_string, str5, 3);
+    my_strncat(my_string, str5, 3);
     cout << "\nLine 8: my_string contains:\"" << my_string << "\"";
   
-    length = (int) strlen(my_string);
+    length = (int) my_strlen(my_string);
     cout << "\nLine 9: my_string length is " << length << ".";
    
-    strncat(my_string, str2,  4);
+    my_strncat(my_string, str2,  4);
     cout << "\nLine 10: my_string contains:\"" << my_string << "\"";
     
     /* strncat append ONLY up ot '\0' character from str3 -- not 6 characters */
-    strncat(my_string, str3, 6);
+    my_strncat(my_string, str3, 6);
     cout << "\nLine 11: my_string contains:\"" << my_string << "\"";
    
-    length = (int) strlen(my_string);
+    length = (int) my_strlen(my_string);
     cout << "\nLine 12; my_string has " << length << " characters.";
 
     cout << "\n\nUsing strcmp - C library function: ";
@@ -90,4 +98,32 @@ int main(void)
     cout << "\n\"Orange\" is greater than \"Apple\" ... strcmp returns: " <<
     strcmp("Orange", "Apple") << endl;
     return 0;
+}
+
+int my_strlen(const char *s) {
+    int length = 0;
+    //Increment length and pointer value until '\0'
+    while(*s != '\0') {
+        length ++;
+        s ++;
+    }
+    return length;
+}
+
+void my_strncat(char *dest, const char *source, int numChars) {
+    //Increment dest until the end of the string is reached.
+    while(*dest != '\0') { dest++; }
+    
+    //Loop until numChars characters are copied or the end of source is reached
+    while(numChars && *source != '\0') {
+        //Set source char to dest char
+        *dest = *source;
+        //cout << "Source is " << *source << " New dest is " << *dest << endl;
+        source ++;
+        dest ++;
+        //Lower numChars until it reaches 0
+        numChars --;
+    }
+    //Make dest a valid C-string by appending '\0' at the end
+    *dest = '\0';
 }
